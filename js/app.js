002bb4bdec326c0257c8097aaec6362c549ede16
Julio Saraiva <contato@juliosaraiva.com.br>
2019-08-14 05:46:28 -0300

    Created modal

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
-    produto: false
+    produto: "",
+    evento: ""
   },
   filters: {
     changeValue(value) {
       return value.toLocaleString("en-US", {
         style: "currency",
-        currency: "usd"
+        currency: "USD"
       });
     },
     upperCase(value) {
@@ -25,6 +26,18 @@ const vm = new Vue({
       fetch(`./api/produtos/${id}/dados.json`)
         .then(resp => resp.json())
         .then(json => (this.produto = json));
+    },
+    abrirModal(id) {
+      this.getProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.produto = false;
+      }
     }
   },
   created() {
