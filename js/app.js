0f3893380f2aef543ff2cc5fea481cea861030f7
Julio Saraiva <contato@juliosaraiva.com.br>
2019-08-15 04:50:05 -0300

    Carrinho & LocalStorage

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: "",
-    evento: ""
+    evento: "",
+    carrinho: []
   },
   filters: {
     changeValue(value) {
@@ -14,6 +15,17 @@ const vm = new Vue({
     },
     upperCase(value) {
       return value.toUpperCase();
+    }
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+      }
+      return total;
     }
   },
   methods: {
@@ -38,9 +50,27 @@ const vm = new Vue({
       if (target === currentTarget) {
         this.produto = false;
       }
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+    verificaLocalStorage() {
+      if (window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
+    }
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
     }
   },
   created() {
-    this.getProdutos();
+    this.getProdutos(), this.verificaLocalStorage();
   }
 });
